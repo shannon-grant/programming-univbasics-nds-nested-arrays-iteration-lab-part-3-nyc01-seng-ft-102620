bf910d9d221222f18a438535e2679484bf108683
Shannon Grant <grant.shannon90@gmail.com>
2020-10-19 12:59:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,6 +10,7 @@ def join_nested_strings(src)
       if src[row_index][element_index].include?("") do
         new_string += src[row_index][element_index]
       end
+      end
         #join together to make new string
       element_index +=1
     end
